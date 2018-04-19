bc770fe9e51baf429e1722539d6347cac3f6e5de
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-19 11:36:17 +0900

    alarm toast

--- UiAlertControlker01/ViewController.swift
+++ UiAlertControlker01/ViewController.swift
@@ -15,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btp(_ sender: Any) {
+        
+        let myAlertController = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다.", preferredStyle: .actionSheet)
+        
+        //AlertAction 만들기
+        let okAction = UIAlertAction(title: "확인", style: .default){(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red}
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { (myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white})
+        let testAction = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
+            
+            //AlertAction을 AlertController에 넣기
+            myAlertController.addAction(okAction)
+            myAlertController.addAction(cancelAction)
+            myAlertController.addAction(testAction)
+            
+            //화면에 출력
+            present(myAlertController, animated: true, completion: nil)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
